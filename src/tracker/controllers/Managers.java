5b3496a1a00801f47d7e1848e5e409ef909a423e
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-04-18 00:20:49 +0300

    Made private constructor

--- src/tracker/controllers/Managers.java
+++ src/tracker/controllers/Managers.java
@@ -1,6 +1,8 @@
 package tracker.controllers;
 
 public class Managers {
+
+    private Managers(){}
     public static TaskManager getDefault(){
         return new InMemoryTaskManager(getDefaultHistory());
     }
