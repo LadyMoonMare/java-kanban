549c37605ad89fe21292b3c3ed7a9cde423e4ab7
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-04-18 14:07:47 +0300

    Fix checkstyle

--- src/tracker/controllers/Managers.java
+++ src/tracker/controllers/Managers.java
@@ -2,11 +2,15 @@ package tracker.controllers;
 
 public class Managers {
 
-    private Managers(){}
-    public static TaskManager getDefault(){
+    private Managers() {
+
+    }
+
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager(getDefaultHistory());
     }
-    public static HistoryManager getDefaultHistory(){
+
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 }
