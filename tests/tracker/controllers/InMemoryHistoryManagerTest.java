5fd99acce6be9915fc26c14fc217e7fe90d37b22
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-04-18 14:31:58 +0300

    Create creation success test

--- tests/tracker/controllers/InMemoryHistoryManagerTest.java
+++ tests/tracker/controllers/InMemoryHistoryManagerTest.java
@@ -10,13 +10,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
 class InMemoryHistoryManagerTest {
+
+    @Test
+    void shouldCreateHistory() {
+        TaskManager manager = Managers.getDefault();
+        Task task = new Task("Task", "Description", Status.NEW);
+        manager.addTask(task);
+
+        assertTrue(manager.getHistory().isEmpty());
+
+        manager.getTask(task.getId());
+        Task[] referArray = {task};
+        assertNotNull(manager.getHistory());
+        assertArrayEquals(referArray,manager.getHistory().toArray());
+    }
+
     @Test
      void shouldSaveOnlyOneCopyOfObjectInHistory() {
         TaskManager manager = Managers.getDefault();
         Task task = new Task("Task", "Description", Status.NEW);
         manager.addTask(task);
-        manager.getTask(task.getId());
 
+        manager.getTask(task.getId());
         List<Task> refer = manager.getHistory();
         task.setStatus(Status.IN_PROGRESS);
         manager.updateTask(task);
