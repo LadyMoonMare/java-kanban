aba2aa907cede28ba4e0f76ab18f7ae6e3b0edd9
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-04-18 14:10:50 +0300

    Create deletion success test

--- tests/tracker/controllers/InMemoryHistoryManagerTest.java
+++ tests/tracker/controllers/InMemoryHistoryManagerTest.java
@@ -1,7 +1,9 @@
 package tracker.controllers;
 
 import org.junit.jupiter.api.Test;
+import tracker.model.Epic;
 import tracker.model.Status;
+import tracker.model.Subtask;
 import tracker.model.Task;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +11,7 @@ import java.util.List;
 
 class InMemoryHistoryManagerTest {
     @Test
-    public void shouldSaveAllCopiesOfObjectInHistory() {
+     void shouldSaveOnlyOneCopyOfObjectInHistory() {
         TaskManager manager = Managers.getDefault();
         Task task = new Task("Task", "Description", Status.NEW);
         manager.addTask(task);
@@ -20,24 +22,47 @@ class InMemoryHistoryManagerTest {
         manager.updateTask(task);
         manager.getTask(task.getId());
 
-        assertNotEquals(refer, manager.getHistory());
+        assertEquals(refer, manager.getHistory());
     }
 
     @Test
-    public void shouldBePositiveIfHistorySizeIs10AndLess() {
+    void shouldBePositiveIfTaskHistoryClearanceSuccess() {
         TaskManager manager = Managers.getDefault();
         Task task = new Task("Task", "Description", Status.NEW);
+        Task task1 = new Task("Task", "Description", Status.NEW);
+        Epic epic = new Epic("Epic", "Description");
+        manager.addEpic(epic);
+        Subtask subtask0 = new Subtask("Subtask0", "Description", Status.NEW,
+                epic.getId());
+
         manager.addTask(task);
-        for (int i = 0; i < 9; i++) {
-            manager.getTask(task.getId());
-        }
+        manager.addTask(task1);
+        manager.addSubtask(subtask0);
 
-        assertEquals(9, manager.getHistory().size());
-
+        Task[] referArray = {task1, task, epic, subtask0};
         manager.getTask(task.getId());
-        assertEquals(10, manager.getHistory().size());
-
+        manager.getTask(task1.getId());
+        manager.getTask(task1.getId());
         manager.getTask(task.getId());
-        assertEquals(10, manager.getHistory().size());
+        manager.getEpic(epic.getId());
+        manager.getSubtask(subtask0.getId());
+
+        assertArrayEquals(referArray, manager.getHistory().toArray());
+
+        manager.removeTask(task.getId());
+        Task[] newReferArray = {task1, epic, subtask0};
+        assertArrayEquals(newReferArray, manager.getHistory().toArray());
+
+        manager.removeTask(task1.getId());
+        Task[] referEpicArray = {epic, subtask0};
+        assertArrayEquals(referEpicArray, manager.getHistory().toArray());
+
+        manager.removeSubtask(subtask0.getId());
+        Task[] newReferEpicArray = {epic};
+        assertArrayEquals(newReferEpicArray, manager.getHistory().toArray());
+        assertTrue(epic.getSubtasks().isEmpty());
+
+        manager.removeEpic(epic.getId());
+        assertTrue(manager.getHistory().isEmpty());
     }
 }
