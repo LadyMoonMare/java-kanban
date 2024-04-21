646c624ed6baa128e9b216db53a07274a4663065
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-04-22 01:14:32 +0300

    Put clearense test to fix git

--- test/tracker/controllers/InMemoryHistoryManagerTest.java
+++ test/tracker/controllers/InMemoryHistoryManagerTest.java
@@ -39,4 +39,45 @@ class InMemoryHistoryManagerTest {
 
         assertEquals(refer, manager.getHistory());
     }
+
+    @Test
+    void shouldBePositiveIfTaskHistoryClearanceSuccess() {
+        TaskManager manager = Managers.getDefault();
+        Task task = new Task("Task", "Description", Status.NEW);
+        Task task1 = new Task("Task1", "Description", Status.NEW);
+        Epic epic = new Epic("Epic", "Description");
+        manager.addEpic(epic);
+        Subtask subtask0 = new Subtask("Subtask0", "Description", Status.NEW,
+                epic.getId());
+
+        manager.addTask(task);
+        manager.addTask(task1);
+        manager.addSubtask(subtask0);
+
+        Task[] referArray = {task1, task, epic, subtask0};
+        manager.getTask(task.getId());
+        manager.getTask(task1.getId());
+        manager.getTask(task1.getId());
+        manager.getTask(task.getId());
+        manager.getEpic(epic.getId());
+        manager.getSubtask(subtask0.getId());
+
+        assertArrayEquals(referArray, manager.getHistory().toArray());
+
+        manager.removeTask(task.getId());
+        Task[] newReferArray = {task1, epic, subtask0};
+        assertArrayEquals(newReferArray, manager.getHistory().toArray());
+
+        manager.removeTask(task1.getId());
+        Task[] referEpicArray = {epic, subtask0};
+        assertArrayEquals(referEpicArray, manager.getHistory().toArray());
+
+        manager.removeSubtask(subtask0.getId());
+        Task[] newReferEpicArray = {epic};
+        assertArrayEquals(newReferEpicArray, manager.getHistory().toArray());
+        assertTrue(epic.getSubtasks().isEmpty());
+
+        manager.removeEpic(epic.getId());
+        assertTrue(manager.getHistory().isEmpty());
+    }
 }
