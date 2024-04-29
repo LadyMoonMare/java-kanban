a3602567ecd5ac1fbd977c00e84ad0f28f0bb88a
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-04-29 15:09:35 +0300

    Make class for toString and fromString methods

--- src/tracker/util/StringManager.java
+++ src/tracker/util/StringManager.java
@@ -1,2 +1,36 @@
-package tracker.util;public class StringManager {
+package tracker.util;
+
+import tracker.model.*;
+
+public class StringManager {
+    private StringManager() {
+    }
+
+    public static String taskToString(Task task) {
+        return task.getId() + "," + TaskType.TASK + "," + task.getTaskName() + ","
+                + task.getTaskDescription() + "," + task.getStatus();
+    }
+
+    public static String epicToString(Epic epic) {
+        return epic.getId() + "," + TaskType.EPIC + "," + epic.getTaskName() + ","
+                + epic.getTaskDescription() + "," + epic.getStatus();
+    }
+
+    public static String subtaskToString(Subtask subtask) {
+        return subtask.getId() + "," + TaskType.SUBTASK + "," + subtask.getTaskName() + ","
+                + subtask.getTaskDescription() + "," + subtask.getStatus() + "," + subtask.getEpicId();
+    }
+
+    public static Task taskFromString(String[] parts) {
+        return new Task(parts[2], parts[4], Integer.parseInt(parts[0]), Status.valueOf(parts[3]));
+    }
+
+    public static Epic epicFromString(String[] parts) {
+        return new Epic(parts[2], parts[4], Integer.parseInt(parts[0]), Status.valueOf(parts[3]));
+    }
+
+    public static Subtask subtaskFromString(String[] parts) {
+        return new Subtask(parts[2], parts[4], Integer.parseInt(parts[0]), Status.valueOf(parts[3]),
+                Integer.parseInt(parts[5]));
+    }
 }
