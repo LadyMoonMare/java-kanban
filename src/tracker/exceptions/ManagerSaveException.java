bcda4e659cd1ce3d38d102388d1428e605b96f37
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-05-01 17:41:28 +0300

    Refactor to new package

--- src/tracker/exceptions/ManagerSaveException.java
+++ src/tracker/exceptions/ManagerSaveException.java
@@ -1,6 +1,6 @@
-package tracker.controllers;
+package tracker.exceptions;
 
-public class ManagerSaveException extends Error {
+public class ManagerSaveException extends RuntimeException {
 
     public ManagerSaveException() {
         super();
