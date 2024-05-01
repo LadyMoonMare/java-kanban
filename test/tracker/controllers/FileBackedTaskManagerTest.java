d9824866d698db60b10f6033033f654a21b71b4b
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-05-01 17:46:04 +0300

    Fix tests bodies from bugs

--- test/tracker/controllers/FileBackedTaskManagerTest.java
+++ test/tracker/controllers/FileBackedTaskManagerTest.java
@@ -12,106 +12,115 @@ import static org.junit.jupiter.api.Assertions.*;
 class FileBackedTaskManagerTest {
     @Test
     void shouldBePositiveIfEmptyFileSaveAndLoadSuccess () throws IOException {
-        try {
-            File file = File.createTempFile("temp","csv", new File( "/resources") );
-            File file1 = File.createTempFile("temp1","csv", new File( "/resources") );
+            File file = File.createTempFile("temp","csv", new File( "C:/resources") );
+            File file1 = File.createTempFile("temp1","csv", new File( "C:/resources") );
             FileBackedTaskManager fb = new FileBackedTaskManager(file.toPath());
             assertNotNull(fb);
             FileBackedTaskManager fb1 = FileBackedTaskManager.loadFromFile(file1);
             assertNotNull(fb1);
-        } catch (IOException e) {
-            e.getStackTrace();
-        }
     }
 
     @Test
     void shouldBePositiveIfTasksSaveIsCorrect() throws IOException {
-        try {
-            File file = File.createTempFile("temp","csv", new File( "/resources") );
-            Task task = new Task("task1", "desc1");
-            Epic epic = new Epic("epic1","epicDesk");
-            Subtask subtask = new Subtask("subtask1", "subtaskDesc",
-                    Status.NEW, epic.getId());
+            File file = File.createTempFile("temp","csv", new File( "C:/resources"));
             FileBackedTaskManager fb = new FileBackedTaskManager(file.toPath());
-            assertNotNull(fb);
+            Task task = new Task("task1", "desc1", Status.NEW);
+            Epic epic = new Epic("epic1","epicDesk");
+            final String COLUMNS = "id, type, name, status, description, epic";
 
             fb.addTask(task);
             fb.getTask(task.getId());
             fb.addEpic(epic);
             fb.getEpic(epic.getId());
+            Subtask subtask = new Subtask("subtask1", "subtaskDesc",
+                    Status.NEW, epic.getId());
+            assertNotNull(fb);
+
             fb.addSubtask(subtask);
             fb.getSubtask(subtask.getId());
 
-            String refer = String.valueOf(task.getId()) + TaskType.TASK + task.getTaskName()
-                    + task.getStatus() + task.getTaskDescription();
-            refer = refer + String.valueOf(epic.getId()) + TaskType.EPIC + epic.getTaskName()
-                    + epic.getStatus() + epic.getTaskDescription();
-            refer = refer + String.valueOf(subtask.getId()) + TaskType.SUBTASK + subtask.getTaskName()
-                    + subtask.getStatus() + subtask.getTaskDescription()
-                    + String.valueOf(subtask.getEpicId());
-            refer = refer + String.valueOf(task.getId()) + String.valueOf(epic.getId())
-                    + String.valueOf(subtask.getId());
-            BufferedReader bf = new BufferedReader(new FileReader(file));
-            String test = "";
-            while (bf.ready()) {
-                test = bf.readLine();
+            String refer = COLUMNS;
+            refer = refer + String.valueOf(task.getId()) + "," + TaskType.TASK + ","  + task.getTaskName()
+                    + "," + task.getStatus() + ","  + task.getTaskDescription();
+            refer = refer + String.valueOf(epic.getId()) + ","  + TaskType.EPIC + ","  + epic.getTaskName()
+                    + "," + epic.getStatus() + ","  + epic.getTaskDescription();
+            refer = refer + String.valueOf(subtask.getId()) + ","  + TaskType.SUBTASK + ","
+                    + subtask.getTaskName() + ","  + subtask.getStatus() + ","  +
+                    subtask.getTaskDescription() + ","  + String.valueOf(subtask.getEpicId());
+            StringBuilder history = new StringBuilder();
+            for (Task t : fb.manager.getHistory()) {
+                 history.append(t.getId()).append(",");
             }
-            bf.close();
-            assertEquals(refer,test);
+            refer = refer + history;
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            StringBuilder test = new StringBuilder();
+            while (br.ready()) {
+                test.append(br.readLine());
+            }
+            br.close();
+            assertEquals(refer, test.toString());
 
             fb.removeEpic(epic.getId());
-            refer = String.valueOf(task.getId()) + TaskType.TASK + task.getTaskName()
-                    + task.getStatus() + task.getTaskDescription() + String.valueOf(task.getId());
-
-            bf = new BufferedReader(new FileReader(file));
-            test = "";
-            while (bf.ready()) {
-                test = bf.readLine();
+            refer = COLUMNS + String.valueOf(task.getId()) + "," + TaskType.TASK + "," + task.getTaskName()
+                    + ","+ task.getStatus() + "," + task.getTaskDescription();
+            history = new StringBuilder();
+            for (Task t : fb.manager.getHistory()) {
+                    history.append(t.getId()).append(",");
             }
-            bf.close();
-            assertEquals(refer,test);
+            refer = refer + history;
+
+            br = new BufferedReader(new FileReader(file));
+            test = new StringBuilder();
+            while (br.ready()) {
+                test.append(br.readLine());
+            }
+            br.close();
+            assertEquals(refer, test.toString());
 
             task.setStatus(Status.DONE);
             fb.updateTask(task);
-            refer = String.valueOf(task.getId()) + TaskType.TASK + task.getTaskName()
-                    + Status.DONE + task.getTaskDescription() + String.valueOf(task.getId());
-
-            bf = new BufferedReader(new FileReader(file));
-            test = "";
-            while (bf.ready()) {
-                test = bf.readLine();
+            refer = COLUMNS + String.valueOf(task.getId()) + "," + TaskType.TASK + "," + task.getTaskName()
+                    + ","+ Status.DONE + "," + task.getTaskDescription();
+            history = new StringBuilder();
+            for (Task t : fb.manager.getHistory()) {
+                    history.append(t.getId()).append(",");
             }
-            bf.close();
-            assertEquals(refer,test);
-        } catch (IOException e) {
-            e.getStackTrace();
-        }
+            refer = refer + history;
+
+            br = new BufferedReader(new FileReader(file));
+            test = new StringBuilder();
+            while (br.ready()) {
+                test.append(br.readLine());
+            }
+            br.close();
+            assertEquals(refer, test.toString());
     }
 
     @Test
-    void Test3() throws IOException {
-        try {
-            File file = File.createTempFile("temp","csv", new File( "/resources") );
-            Task task = new Task("task1", "desc1");
-            Epic epic = new Epic("epic1","epicDesk");
-            Subtask subtask = new Subtask("subtask1", "subtaskDesc",
-                    Status.NEW, epic.getId());
+    void shouldBePositiveIfLoadSuccess() throws IOException {
+            File file = File.createTempFile("temp","csv", new File( "C:/resources") );
             FileBackedTaskManager fb = new FileBackedTaskManager(file.toPath());
-            assertNotNull(fb);
-
+            Task task = new Task("task1", "desc1", Status.NEW);
+            Epic epic = new Epic("epic1","epicDesk");
             fb.addTask(task);
             fb.getTask(task.getId());
             fb.addEpic(epic);
             fb.getEpic(epic.getId());
+
+            Subtask subtask = new Subtask("subtask1", "subtaskDesc",
+                    Status.NEW, epic.getId());
+            assertNotNull(fb);
+            assertNotNull(epic.getStatus());
+
             fb.addSubtask(subtask);
             fb.getSubtask(subtask.getId());
 
             FileBackedTaskManager fb1 = FileBackedTaskManager.loadFromFile(file);
-            assertEquals(fb.getTasks(), fb1.getTasks());
-            assertEquals(fb.getEpics(), fb1.getEpics());
-            assertEquals(fb.getSubtasks(), fb1.getSubtasks());
-        } catch (IOException e) {
-            e.getStackTrace();
-        }
+            assertEquals(fb.getTasks().toString(), fb1.getTasks().toString());
+            assertEquals(fb.getEpics().toString(), fb1.getEpics().toString());
+            assertEquals(fb.getSubtasks().toString(), fb1.getSubtasks().toString());
+            assertEquals(fb.manager.getHistory().toString(), fb1.manager.getHistory().toString());
+
     }
 }
