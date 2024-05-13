2f74ab790122eb8148edfdeddf41b4a87360c4a0
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-05-14 01:38:43 +0300

    Add server folder and HttpTaskServerClass with method main

--- src/tracker/server/HttpTaskServer.java
+++ src/tracker/server/HttpTaskServer.java
@@ -1,23 +1,53 @@
+package tracker.server;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpServer;
 import tracker.controllers.Managers;
 import tracker.controllers.TaskManager;
+import tracker.gsonAdapters.*;
 import tracker.httphandlers.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class HttpTaskServer {
     private static final int PORT = 8080;
+    private HttpServer httpServer;
+    private static Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+            .registerTypeAdapter(Duration.class, new DurationAdapter())
+            .create();
 
-        public static void main(String[] args) throws IOException {
-            HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
-            TaskManager manager = Managers.getDefault();
-            httpServer.createContext("/tasks", new TaskHandler(manager));
-            httpServer.createContext("/subtasks", new SubtaskHandler(manager));
-            httpServer.createContext("/history", new HistoryHandler(manager));
-            httpServer.createContext("/prioritized", new PrioritizedHandler(manager));
+    public static Gson getGson() {
+        return gson;
+    }
 
+    public HttpTaskServer(TaskManager manager) throws IOException {
+        httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+        httpServer.createContext("/tasks", new TaskHandler(manager));
+        httpServer.createContext("/subtasks", new SubtaskHandler(manager));
+        httpServer.createContext("/history", new HistoryHandler(manager));
+        httpServer.createContext("/prioritized", new PrioritizedHandler(manager));
+        httpServer.createContext("/epics", new EpicHandler(manager));
+    }
 
+    public static void main(String[] args) throws IOException {
+        TaskManager manager = Managers.getDefault();
+        HttpTaskServer server = new HttpTaskServer(manager);
 
-        }
+    }
+
+    public void start() {
+        httpServer.start();
+        System.out.println("HTTP-сервер запущен на " + PORT + " порту.");
+    }
+
+    public void stop() {
+        httpServer.stop(0);
+        System.out.println("HTTP-сервер остановлен.");
+    }
+
 }
