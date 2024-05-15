b4e644228b1eaaf2dc22bd7caa993a1caad754cc
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-05-15 09:45:27 +0300

    Add getDefaultServer() method

--- src/tracker/controllers/Managers.java
+++ src/tracker/controllers/Managers.java
@@ -1,9 +1,12 @@
 package tracker.controllers;
 
+import tracker.server.HttpTaskServer;
+
+import java.io.IOException;
+
 public class Managers {
 
     private Managers() {
-
     }
 
     public static TaskManager getDefault() {
@@ -12,5 +15,13 @@ public class Managers {
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    public static HttpTaskServer getDefaultServer() throws IOException {
+        return new HttpTaskServer(getDefault());
+    }
+
+    public static HttpTaskServer getDefaultServer(TaskManager manager) throws IOException {
+        return new HttpTaskServer(manager); //метод для возможности тестирования
     }
 }
